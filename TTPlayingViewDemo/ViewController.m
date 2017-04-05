883f99c0627802c51b8bfd534038c3626a0258d6
jiang <e>
2017-04-05 16:44:32 +0800

    Initial Commit

---
+++ TTPlayingViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TTPlayingViewDemo
+//
+//  Created by jiang on 2017/4/5.
+//  Copyright © 2017年 jiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
